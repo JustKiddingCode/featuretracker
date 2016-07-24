cff36db26870b785975c79d39bd6a0d269ba92dd
Konstantin Zangerle <info@konstantinzangerle.de>
2016-07-24 17:17:14 +0200

    db design to sql

---
+++ sql/00_init.sql
@@ -0,0 +1,60 @@
+
+CREATE TABLE Queue
+(
+	Queue-ID SERIAL PRIMARY KEY,
+	Name text,
+	auto-close boolean
+);
+
+
+CREATE TABLE Queue_NoTicket
+(
+	Queue-ID integer REFERENCES Queue(Queue-ID),
+	EMail_Regex text
+);
+
+CREATE TABLE Queue_Admin
+(
+	Queue-ID integer REFERENCES Queue(Queue-ID),
+	Email_Regex text
+);
+
+CREATE TABLE Status
+(
+	Status-ID SERIAL PRIMARY KEY,
+	Name text
+);
+
+
+CREATE TABLE Tickets 
+(
+	Ticket-ID SERIAL PRIMARY KEY,
+	Status integer REFERENCES Status(Status-ID),
+	Owner VARCHAR (254),
+	Originator VARCHAR(254),
+	Queue integer REFERENCES Queue(Queue-ID),
+	Subject text,
+	opened TIMESTAMP WITH TIME ZONE
+);
+
+
+CREATE TABLE Ticket_Dependencies
+(
+	Ticket-ID REFERENCES Tickets(Ticket-ID),
+	depends integer
+);
+
+CREATE TABLE EMails
+(
+	Message-ID text PRIMARY KEY,
+	Content text
+);
+
+CREATE TABLE Link_Ticket_Message 
+(
+	Ticket-ID REFERENCES Tickets(Ticket-ID),
+	Message-ID REFERENCES EMails(Message-ID)
+);
+
+
+
